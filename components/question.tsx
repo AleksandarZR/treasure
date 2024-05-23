036d9e1710e36dfd84f8e7ad6186124450824941
Aleksandar Pletl <aleksandar.pletl@42computing.tech>
2024-05-23 20:16:52 +0200

    Sredjen kod

--- components/question.tsx
+++ components/question.tsx
@@ -2,39 +2,18 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "./question.module.css";
-import { useRouter } from 'next/router';
 
 interface Props {
     questionText: string,
-    answerText: string | undefined,
+    answerText: string,
     answerExpected: string,
     cipher: string
 };
 
 export default function Question(props: Props) {
-    const [isCipherVisible, setIsCipherVisible] = useState(false);
     const [answer, setAnswer] = useState("");
 
-    useEffect(() => {
-        console.log("useEffect called");
-
-        props.answerText = (document.getElementById(props.answerExpected) as HTMLInputElement | null)?.value;
-        if (props.answerText) {
-            if (props.answerText === props.answerExpected) {
-                setIsCipherVisible(true);
-            } else {
-                setIsCipherVisible(false);
-            }
-        }
-    }, [props.answerText]);
-
     const isAnswerCorrect = () => {
-        /*console.log("called");
-        if (isCipherVisible) {
-            return true;
-        } else {
-            return false;
-        }*/
         if (answer === props.answerExpected) {
             return true;
         } else {
@@ -42,31 +21,13 @@ export default function Question(props: Props) {
         }
     }
 
-    const returnStyle = () => {
-        /*if (isCipherVisible) {
-            return styles.cipherVisible;
-        } else {
-            return styles.cipherInvisible;
-        }*/
-
-        const answer = (document.getElementById(props.answerExpected) as HTMLInputElement | null)?.value;
-        if (answer) {
-            if (answer === props.answerExpected) {
-                return styles.cipherVisible;
-            } else {
-                return styles.cipherInvisible;
-            }
-        }
-
-        styles.cipherInvisible;
-    }
-
     return (
         <div className={styles.main}>
             <p className={styles.question}>{props.questionText}</p>
             <input className={styles.answer} type="text" id={props.answerExpected} value={answer} onChange={e => setAnswer(e.target.value)}></input>
-            <div className={isAnswerCorrect() ? styles.cipherVisible : styles.cipherInvisible}>{props.cipher} </div>
-            {/* <div className={returnStyle()}>{props.cipher}</div>  */}
+            <span className={isAnswerCorrect() ? styles.cipherVisible : styles.cipherInvisible}>
+                <span>{props.cipher}</span>
+            </span>
         </div>
     );
 }
