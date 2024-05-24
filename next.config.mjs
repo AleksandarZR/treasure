3ff6e22f45cb8df70c3bd263788c5abff917f22b
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-05-24 10:23:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/treasure",
-    output: "export",  // <=== enables static exports
+    basePath: "/github-pages",
+    //output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
 
