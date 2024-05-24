48d80ad76b6b728347bd4ab4c1dcfa8b75a2001d
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-05-24 11:09:33 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        loader: "akamai",
-        path: "",
-    },
-    assetPrefix: "./",
+    basePath: '/treasure',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 //export default nextConfig;
